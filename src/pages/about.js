3ceffee3c2825bb8b1ae9f77092f7274792410ae
Trang Le <lethutrang101@gmail.com>
2020-04-08 21:20:03 +0700

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import Title from "../components/title"
+
+export default () => (
+  <Layout>
+    <Title text="About Me" />
+    <div>
+      <Link href="/">Home</Link> | <Link href="/about">About</Link>
+    </div>
+    <p>Hello world!</p>
+  </Layout>
+)
