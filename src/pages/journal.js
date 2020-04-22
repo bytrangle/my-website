7ec7c6e87e0314a56427e494049ad82bb4bc80c2
Trang Le <lethutrang101@gmail.com>
2020-04-22 17:28:39 +0700

    Render a list of Wordpress and Markdown posts

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -8,58 +8,81 @@ export default ({ data }) => {
     ...allMarkdownRemark.edges.map(e => e.node),
     ...allWordpressPost.edges.map(e => e.node),
   ]
-  return allPosts.map(node => {
-    return (
-      <Layout>
-        <div>
-          <h1>
-            {(() => {
-              let result
-              switch (node.internal.type) {
-                case "MarkdownRemark":
-                  result = node.frontmatter.title
-                  break
-                case "wordpress__POST":
-                  result = node.title
-              }
-              return result
-            })()}
-          </h1>
-        </div>
-      </Layout>
-    )
-  })
+  return (
+    <Layout>
+      {allPosts.map(node => {
+        return (
+          <div key={node.id}>
+            <Link
+              to={(() => {
+                let result
+                switch (node.internal.type) {
+                  case "MarkdownRemark":
+                    result = node.fields.link
+                    break
+                  case "wordpress__POST":
+                    result = node.fields.link
+                }
+                return result
+              })()}
+            >
+              <h1 className="headline">
+                {(() => {
+                  let result
+                  switch (node.internal.type) {
+                    case "MarkdownRemark":
+                      result = node.frontmatter.title
+                      break
+                    case "wordpress__POST":
+                      result = node.title
+                  }
+                  return result
+                })()}
+              </h1>
+            </Link>
+          </div>
+        )
+      })}
+    </Layout>
+  )
 }
 export const pageQuery = graphql`
-    query {
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-            edges {
-                node {
-                    id
-                    frontmatter {
-                        title
-                        date(formatString: 'DD MMMM, YYYY')
-                    }
-                    fields {
-                        slug
-                    }
-                    internal {
-                        type
-                    }
-                }
-            }
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+            link
+          }
+          internal {
+            type
+          }
         }
-        allWordpressPost(sort: {fields: date}) {
-            edges {
-                node {
-                    id
-                    title
-                    internal {
-                        type
-                    }
-                    slug
-                }
-            }
-        }
+      }
     }
+    allWordpressPost(
+      sort: { fields: date, order: DESC }
+      filter: { format: { eq: "standard" } }
+    ) {
+      edges {
+        node {
+          id
+          title
+          date
+          internal {
+            type
+          }
+          fields {
+            link
+          }
+        }
+      }
+    }
+  }
 `
