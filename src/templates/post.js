311597248b87918c952887f41df0822613275de0
Trang Le <lethutrang101@gmail.com>
2020-04-22 17:28:11 +0700

    Render Wordpress and Markdown

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,15 +1,40 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 import Title from "../components/title"
 import styles from "./post.module.scss"
 
 export default ({ data }) => {
-  const post = data.markdownRemark
   const { markdownRemark, wordpressPost } = data
   const { featuredImage } = markdownRemark.frontmatter
   return (
     <Layout>
+      {wordpressPost && (
+        <article className="col-12">
+          <Title text={wordpressPost.title}></Title>
+          <div
+            style={{
+              width: "100%",
+              height: "200px",
+              backgroundColor: "#fafafa",
+              backgroundImage:
+                "Url(" + wordpressPost.featured_media.source_url + ")",
+              backgroundSize: "cover",
+              backgroundRepat: "no-repat",
+              marginBottom: "30px",
+            }}
+          ></div>
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: wordpressPost.content }}
+          />
+        </article>
+      )}
+      {markdownRemark && (
+        <article className="col-12">
+          <div className="frontmatter max-width-3 mx-auto mb-3">
+            <div className="img__container flex justify-end width-full">
               {featuredImage !== null && (
                 <Img
                   fluid={featuredImage.childImageSharp.fluid}
@@ -20,6 +45,16 @@ export default ({ data }) => {
                   placeholderStyle={{ paddingBottom: "0" }}
                 />
               )}
+            </div>
+            <Title text={markdownRemark.frontmatter.title}></Title>
+          </div>
+
+          <div
+            className="post-content serif line-height-5"
+            dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
+          />
+        </article>
+      )}
     </Layout>
   )
 }
