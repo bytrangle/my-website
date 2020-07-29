b299ac5263339a0b335579a6d9dadab7c0bac462
Trang Le <lethutrang101@gmail.com>
2020-07-29 11:16:02 +0700

    Remove retrieval of Wordpress data

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,31 +6,10 @@ import Title from "../components/title"
 import styles from "./post.module.scss"
 
 export default ({ data }) => {
-  const { markdownRemark, wordpressPost } = data
+  const { markdownRemark } = data
   const { featuredImage } = markdownRemark.frontmatter
   return (
     <Layout>
-      {wordpressPost && (
-        <article className="col-12">
-          <Title text={wordpressPost.title}></Title>
-          <div
-            style={{
-              width: "100%",
-              height: "200px",
-              backgroundColor: "#fafafa",
-              backgroundImage:
-                "Url(" + wordpressPost.featured_media.source_url + ")",
-              backgroundSize: "cover",
-              backgroundRepat: "no-repat",
-              marginBottom: "30px",
-            }}
-          ></div>
-          <div
-            className={styles.content}
-            dangerouslySetInnerHTML={{ __html: wordpressPost.content }}
-          />
-        </article>
-      )}
       {markdownRemark && (
         <article className="col-12">
           <div className="frontmatter max-width-3 mx-auto mb-3">
@@ -74,13 +53,6 @@ export const query = graphql`
           }
         }
       }
-    }
-    wordpressPost(id: { eq: $id }) {
-      title
-      featured_media {
-        source_url
-      }
-      content
     }
   }
 `
