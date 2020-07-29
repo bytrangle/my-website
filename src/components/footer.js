cb5eb0a010d5a601f6775c0d0bf80af63ded65be
Trang Le <lethutrang101@gmail.com>
2020-07-29 11:16:25 +0700

    Redesigned footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,13 @@ import styles from './footer.module.scss'
 
 export default ({ children }) => (
 
-  <footer className={styles.container}>
-    <div className={styles.footer}>
-      {children}
+  <footer className={`pt3 ${styles.container}`}>
+    <div className={styles.footer}>{children}
+    </div>
+    <div>
+      <a class={`mr2 ${styles.socialMedia__link}`}>Github</a>
+      <a class={`mr2 ${styles.socialMedia__link}`}>Twitter</a>
+      <a class={`mr2 ${styles.socialMedia__link}`}>Email</a>
     </div>
   </footer>
 
