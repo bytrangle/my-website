8da383206a6491d1050e04a0b1b0fe9129ffc725
Trang Le <lethutrang101@gmail.com>
2020-07-30 17:10:58 +0700

    Add class name and change HTML structure of layout design

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import styles from './layout.module.scss'
-import Header from './header.js'
-import Footer from './footer.js'
+import React from "react"
+import "typeface-montserrat"
+import "typeface-ibm-plex-serif"
+import "typeface-ibm-plex-mono"
+import styles from "./layout.module.scss"
+import Header from "./header.js"
+import Footer from "./footer.js"
 
 export default ({ children }) => (
-   <div className={styles.container}>
-      <Header />
-      <div className={styles.content}>
-         {children}
-      </div>
-      <Footer>
-         My new Gatsby Blog 2019
-    </Footer>
-   </div>
+  <div className={`pb3 ${styles.container}`}>
+    <Header />
+    <main className="px3 pb3" role="main">{children}</main>
+<Footer>Made with the awesome static site generator
+  <a href="https://www.gatsbyjs.org"> Gatsby.js</a>
+  </Footer>
+  </div>
 )
-
