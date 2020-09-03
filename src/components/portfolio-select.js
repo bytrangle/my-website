13b079188f1b24ff69da0e8920d78c4c027fd4b2
Trang Le <lethutrang101@gmail.com>
2020-09-03 14:43:44 +0700

    Add portfolio section to home page

---
+++ src/components/portfolio-select.js
@@ -0,0 +1,52 @@
+import React from "react"
+import {StaticQuery, graphql} from "gatsby"
+import Img from "gatsby-image"
+import styles from "./portfolio-select.module.css"
+
+export default () => (
+  <StaticQuery 
+    query={graphql`
+      query Portfolio {
+        allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(portfolio)/.*.md$/"}}) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                description
+                featuredImage {
+                  childImageSharp {
+                    fluid(maxWidth: 800) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+                gif {
+                  relativePath
+                  publicURL
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className="portfolio">
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div className={`${styles.project} items-center mb3`}>
+          {/* <Img 
+            fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
+            style={{ flex: '1', marginLeft: '-6rem'}}
+          /> */}
+          <img className={styles.project__featuredImg} src={node.frontmatter.gif.publicURL} alt="Blah" />
+          <div className={`${styles.project__info} z4 border-thick`}>
+            <h3 className={styles.project__name}>{node.frontmatter.title}</h3>
+            <p className={styles.project__desc}>{node.frontmatter.description}</p>
+          </div>
+          </div>
+        ))}
+      </div>
+    )}
+  />
+)
---
+++ src/portfolio/live-twitter-dashboard-in-google-sheets.md
@@ -0,0 +1,6 @@
+---
+title: "Live Twitter Dashboard in Google Sheets"
+description: "Enhance the everyday Google Sheets with custom scripts to turn it into a real-time Twitter analytics dashboard"
+featuredImage: "33841380766_22f9d7074c_k.jpg"
+gif: "tweet-dashboard-in-google-sheets.gif"
+---
