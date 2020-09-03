f62bc7d7f5bcb8fccff89ae1c7d71a024b62033d
Trang Le <lethutrang101@gmail.com>
2020-09-03 14:34:55 +0700

    Design single post layout similar to Airtable

--- src/components/article.js
+++ src/components/article.js
@@ -1,21 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
-import styles from "./article.module.scss"
+import styles from "./article.module.css"
 
 export default props => (
-  <Link to={props.to}>
-    <article className={styles.articleBox} key={props.id}>
-      <div className={styles.left}>
-        <img
-          src={"https://source.unsplash.com/150x150/?" + props.keywords}
-          alt={props.title}
-        />
-      </div>
-      <div className={styles.right}>
-        <h3>{props.title}</h3>
+  <li className="list-style-none grid-item">
+    <p className="text-blue uppercase sans-serif mb2 bold small">
+      {props.category}
+    </p>
+    <h4 className="big display medium">{props.title}</h4>
+    <p class="sans-serif line-height-4 quiet mb1 grid-txt">{props.excerpt}</p>
+    <Link to={props.to} className="darken1-hover">
+      {/* <div className={styles.left}>
+          <img
+            src={"https://source.unsplash.com/150x150/?" + props.category}
+            alt={props.title}
+          />
+        </div> */}
+      {/* <div className={styles.right}>
+        
         <div className={styles.date}>{props.date}</div>
-        <div>{props.excerpt}</div>
-      </div>
-    </article>
-  </Link>
+        
+      </div> */}
+    </Link>
+  </li>
 )
