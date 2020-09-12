ea211c559d272cd757a14d0dac9b0a5dd65af7e6
Trang Le <lethutrang101@gmail.com>
2020-09-12 22:34:00 +0700

    Move all the site metadata to a new file

---
+++ config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  url: 'https://bytrangle.surge.sh',
+  pathPrefix: '/',
+  title: 'Do less, do better, know why',
+  description: 'Tailored web development to help you achieve more in less time',
+  copyright: '© All rights reserved.',
+  author: {
+    name: 'Trang Le',
+    bio: 'I love to use codes to build digital experiences for an increasingly unpredictable world',
+    contacts: {
+      email: '',
+      twitter: 'https://twitter.com/bytrangle',
+      github: ''
+    }
+  }
+}
