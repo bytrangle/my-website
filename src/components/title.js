e328dd38eabd04cc07cc4b9fb13aaa29b8f03a12
Trang Le <lethutrang101@gmail.com>
2020-09-14 17:56:31 +0700

    Deleted the title component

--- src/components/title.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import styles from "./title.module.scss"
-
-export default props => (
-  <section className="lg-col-8 md-col-10 sm-col-12">
-    <h1 className="huge display line-height-2 strong">{props.text}</h1>
-    <div className={styles.subtitle}>{props.subtitle}</div>
-  </section>
-)
